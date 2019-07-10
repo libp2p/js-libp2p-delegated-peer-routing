ebb23a2d87e44aaf7d2bd56166270b90c71a2940
Alan Shaw <alan.shaw@protocol.ai>
2019-07-10 16:03:44 +0100

    chore: update deps (#10)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 'use strict'
 
-const PeerInfo = require('peer-info')
 const PeerId = require('peer-id')
-const dht = require('ipfs-api/src/dht')
-const defaultConfig = require('ipfs-api/src/utils/default-config')
+const dht = require('ipfs-http-client/src/dht')
+const defaultConfig = require('ipfs-http-client/src/utils/default-config')
 
 const DEFAULT_MAX_TIMEOUT = 30e3 // 30 second default
 const DEFAULT_IPFS_API = {
   protocol: 'https',
   port: 443,
   host: 'ipfs.io'
-}
-
-const peerNotFoundError = (id) => {
-  return new Error(`Peer "${id}" not found`)
 }
 
 class DelegatedPeerRouting {
@@ -49,31 +44,16 @@ class DelegatedPeerRouting {
 
     options.maxTimeout = options.maxTimeout || DEFAULT_MAX_TIMEOUT
 
-    this.dht.findpeer(id, {
+    this.dht.findPeer(id, {
       timeout: `${options.maxTimeout}ms`// The api requires specification of the time unit (s/ms)
-    }, (err, results) => {
+    }, (err, info) => {
       if (err) {
+        if (err.message.includes('not found')) {
+          return callback()
+        }
         return callback(err)
       }
 
-      // cleanup result from ipfs-api
-      const actual = results.filter((res) => Boolean(res.Responses))
-
-      if (actual.length === 0) {
-        return callback(peerNotFoundError(id))
-      }
-
-      const wantedResponse = actual.find((el) => el.Type === 2)
-      if (wantedResponse === undefined) {
-        return callback(peerNotFoundError(id))
-      }
-      const details = wantedResponse.Responses[0]
-      const info = new PeerInfo(
-        PeerId.createFromB58String(details.ID)
-      )
-      details.Addrs.forEach((addr) => info.multiaddrs.add(addr))
-
-      // there should be only one of these
       callback(null, info)
     })
   }
