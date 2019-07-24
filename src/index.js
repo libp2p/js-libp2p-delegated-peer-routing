411111c83aac2fa84a0520e42e2f1318815518cf
Marcin Rataj <lidel@lidel.org>
2019-07-24 11:33:32 +0200

    fix: limit concurrent HTTP requests
    All HTTP requests made by this module are sent to the same delegate
    host. Browsers throttle the number of concurrent requests per hostname,
    right now it is 6 per host, which suffocates the use of delegate and
    blocking it from being used for preload or delegated content routing.
    
    This introduces a task queue that limits the number of concurrent
    requests, making it safe to run in browser context.
    
    See also:
    https://github.com/libp2p/js-libp2p-delegated-content-routing/issues/12
    
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- src/index.js
+++ src/index.js
@@ -3,18 +3,31 @@
 const PeerId = require('peer-id')
 const dht = require('ipfs-http-client/src/dht')
 const defaultConfig = require('ipfs-http-client/src/utils/default-config')
+const { default: PQueue } = require('p-queue')
+const debug = require('debug')
+
+const log = debug('libp2p-delegated-peer-routing')
+log.error = debug('libp2p-delegated-peer-routing:error')
 
 const DEFAULT_MAX_TIMEOUT = 30e3 // 30 second default
 const DEFAULT_IPFS_API = {
   protocol: 'https',
   port: 443,
-  host: 'ipfs.io'
+  host: 'node0.delegate.ipfs.io'
 }
+const CONCURRENT_HTTP_REQUESTS = 4
 
 class DelegatedPeerRouting {
   constructor (api) {
     this.api = Object.assign({}, defaultConfig(), DEFAULT_IPFS_API, api)
     this.dht = dht(this.api)
+
+    // limit concurrency to avoid request flood in web browser
+    // https://github.com/libp2p/js-libp2p-delegated-content-routing/issues/12
+    this._httpQueue = new PQueue({
+      concurrency: CONCURRENT_HTTP_REQUESTS
+    })
+    log(`enabled DelegatedPeerRouting via ${this.api.protocol}://${this.api.host}:${this.api.port}`)
   }
 
   /**
@@ -29,19 +42,22 @@ class DelegatedPeerRouting {
     if (PeerId.isPeerId(id)) {
       id = id.toB58String()
     }
+    log('findPeer starts: ' + id)
 
     options.maxTimeout = options.maxTimeout || DEFAULT_MAX_TIMEOUT
 
     try {
-      return await this.dht.findPeer(id, {
+      return await this._httpQueue.add(() => this.dht.findPeer(id, {
         timeout: `${options.maxTimeout}ms`// The api requires specification of the time unit (s/ms)
-      })
+      }))
     } catch (err) {
       if (err.message.includes('not found')) {
         return undefined
       }
 
       throw err
+    } finally {
+      log('findPeer finished: ' + id)
     }
   }
 }
