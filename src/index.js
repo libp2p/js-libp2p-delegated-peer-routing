7afafd16b838e41a8386d8188c1a39cdfd1531f6
Marcin Rataj <lidel@lidel.org>
2019-07-26 21:18:55 +0200

    fix: limit concurrent HTTP requests
    This is a backport of the fix from
    https://github.com/libp2p/js-libp2p-delegated-peer-routing/pull/12
    that works with 0.2.x
    
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@
 const PeerId = require('peer-id')
 const dht = require('ipfs-http-client/src/dht')
 const defaultConfig = require('ipfs-http-client/src/utils/default-config')
+const { default: PQueue } = require('p-queue')
 
 const DEFAULT_MAX_TIMEOUT = 30e3 // 30 second default
 const DEFAULT_IPFS_API = {
   protocol: 'https',
   port: 443,
-  host: 'ipfs.io'
+  host: 'node0.delegate.ipfs.io'
 }
 
 class DelegatedPeerRouting {
   constructor (api) {
     this.api = Object.assign({}, defaultConfig(), DEFAULT_IPFS_API, api)
     this.dht = dht(this.api)
+    // limit concurrency to avoid request flood in web browser
+    // (backport of: https://github.com/libp2p/js-libp2p-delegated-peer-routing/pull/12)
+    this._httpQueue = new PQueue({ concurrency: 4 })
   }
 
   /**
@@ -44,17 +48,13 @@ class DelegatedPeerRouting {
 
     options.maxTimeout = options.maxTimeout || DEFAULT_MAX_TIMEOUT
 
-    this.dht.findPeer(id, {
+    this._httpQueue.add(() => this.dht.findPeer(id, {
       timeout: `${options.maxTimeout}ms`// The api requires specification of the time unit (s/ms)
-    }, (err, info) => {
-      if (err) {
-        if (err.message.includes('not found')) {
-          return callback()
-        }
-        return callback(err)
+    })).then(res => callback(null, res), err => {
+      if (err.message.includes('not found')) {
+        return callback()
       }
-
-      callback(null, info)
+      return callback(err)
     })
   }
 }
