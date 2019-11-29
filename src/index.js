c6438dd0dabce08fbeb4505406d7bd209fbaf1f2
Vasco Santos <vasco.santos@moxy.studio>
2019-11-29 21:26:43 +0100

    chore: rename timeout option
    BREAKING CHANGE: maxTimeout option renamed to timeout

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 
 const PeerId = require('peer-id')
 const dht = require('ipfs-http-client/src/dht')
-const defaultConfig = require('ipfs-http-client/src/utils/default-config')
+const getEndpointConfig = require('ipfs-http-client/src/get-endpoint-config')
 const { default: PQueue } = require('p-queue')
 const debug = require('debug')
 
 const log = debug('libp2p-delegated-peer-routing')
 log.error = debug('libp2p-delegated-peer-routing:error')
 
-const DEFAULT_MAX_TIMEOUT = 30e3 // 30 second default
+const DEFAULT_TIMEOUT = 30e3 // 30 second default
 const DEFAULT_IPFS_API = {
   protocol: 'https',
   port: 443,
@@ -19,7 +19,7 @@ const CONCURRENT_HTTP_REQUESTS = 4
 
 class DelegatedPeerRouting {
   constructor (api) {
-    this.api = Object.assign({}, defaultConfig(), DEFAULT_IPFS_API, api)
+    this.api = Object.assign({}, getEndpointConfig()(), DEFAULT_IPFS_API, api)
     this.dht = dht(this.api)
 
     // limit concurrency to avoid request flood in web browser
@@ -35,7 +35,7 @@ class DelegatedPeerRouting {
    *
    * @param {PeerID} id
    * @param {object} options
-   * @param {number} options.maxTimeout How long the query can take. Defaults to 30 seconds
+   * @param {number} options.timeout How long the query can take. Defaults to 30 seconds
    * @returns {Promise<PeerInfo>}
    */
   async findPeer (id, options = {}) {
@@ -44,11 +44,11 @@ class DelegatedPeerRouting {
     }
     log('findPeer starts: ' + id)
 
-    options.maxTimeout = options.maxTimeout || DEFAULT_MAX_TIMEOUT
+    options.timeout = options.timeout || DEFAULT_TIMEOUT
 
     try {
       return await this._httpQueue.add(() => this.dht.findPeer(id, {
-        timeout: `${options.maxTimeout}ms`// The api requires specification of the time unit (s/ms)
+        timeout: `${options.timeout}ms`// The api requires specification of the time unit (s/ms)
       }))
     } catch (err) {
       if (err.message.includes('not found')) {
