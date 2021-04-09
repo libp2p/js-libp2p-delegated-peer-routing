f30177f8c6a7ea646e1715bf15e51fd20f6d7c08
Vasco Santos <vasco.santos@moxy.studio>
2021-04-09 15:02:37 +0200

    chore: update deps

--- .aegir.js
+++ .aegir.js
@@ -4,22 +4,20 @@ const { createServer } = require('ipfsd-ctl')
 let server
 
 module.exports = {
-  hooks: {
-    browser: {
-      pre: async () => {
-        server = createServer({
-          host: '127.0.0.1',
-          port: 57483
-        }, {
-          type: 'go',
-          ipfsHttpModule: require('ipfs-http-client'),
-          ipfsBin: require('go-ipfs').path(),
-          test: true
-        })
+  test: {
+    before: async () => {
+      server = createServer({
+        host: '127.0.0.1',
+        port: 57483
+      }, {
+        type: 'go',
+        ipfsHttpModule: require('ipfs-http-client'),
+        ipfsBin: require('go-ipfs').path(),
+        test: true
+      })
 
-        await server.start()
-      },
-      post: () => server.stop()
-    }
+      await server.start()
+    },
+    after: () => server.stop()
   }
 }
