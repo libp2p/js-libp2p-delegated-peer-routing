76c7ac251e10f043d8e883753b87376a04cdce74
achingbrain <alex@achingbrain.net>
2022-03-15 12:04:50 +0000

    fix: update interfaces
    Updates to latest code from https://github.com/libp2p/js-libp2p-interfaces/pull/180

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,22 @@ import { logger } from '@libp2p/logger'
 import { base58btc } from 'multiformats/bases/base58'
 import PQueue from 'p-queue'
 import defer from 'p-defer'
+import errCode from 'err-code'
+import { Multiaddr } from '@multiformats/multiaddr'
+import { peerIdFromString } from '@libp2p/peer-id'
 import type { PeerId } from '@libp2p/interfaces/peer-id'
 import type { IPFSHTTPClient } from 'ipfs-http-client'
 import type { HTTPClientExtraOptions } from 'ipfs-http-client/types/src/types'
 import type { AbortOptions } from 'ipfs-core-types/src/utils'
+import type { PeerRouting } from '@libp2p/interfaces/peer-routing'
+import type { PeerData } from '@libp2p/interfaces/peer-data'
 
 const log = logger('libp2p-delegated-peer-routing')
 
 const DEFAULT_TIMEOUT = 30e3 // 30 second default
 const CONCURRENT_HTTP_REQUESTS = 4
 
-export class DelegatedPeerRouting {
+export class DelegatedPeerRouting implements PeerRouting {
   private readonly client: IPFSHTTPClient
   private readonly httpQueue: PQueue
 
@@ -44,7 +49,7 @@ export class DelegatedPeerRouting {
   /**
    * Attempts to find the given peer
    */
-  async * findPeer (id: PeerId, options: HTTPClientExtraOptions & AbortOptions = {}) {
+  async findPeer (id: PeerId, options: HTTPClientExtraOptions & AbortOptions = {}) {
     log('findPeer starts: %p', id)
     options.timeout = options.timeout ?? DEFAULT_TIMEOUT
 
@@ -57,21 +62,31 @@ export class DelegatedPeerRouting {
     })
 
     try {
-      yield * this.client.dht.findPeer(id.toString(), {
+      await onStart.promise
+
+      for await (const event of this.client.dht.findPeer(id.toString(), {
         timeout: options.timeout
-      })
+      })) {
+        if (event.name === 'FINAL_PEER') {
+          const peerData: PeerData = {
+            id: peerIdFromString(event.peer.id),
+            multiaddrs: event.peer.multiaddrs.map(ma => new Multiaddr(ma.toString())),
+            protocols: []
+          }
+
+          return peerData
+        }
+      }
     } catch (err: any) {
       log.error('findPeer errored: %o', err)
-
-      if (err.message.includes('not found')) { // eslint-disable-line @typescript-eslint/strict-boolean-expressions
-        return
-      }
 
       throw err
     } finally {
       onFinish.resolve()
       log('findPeer finished: %p', id)
     }
+
+    throw errCode(new Error('Not found'), 'ERR_NOT_FOUND')
   }
 
   /**
@@ -94,9 +109,17 @@ export class DelegatedPeerRouting {
     try {
       await onStart.promise
 
-      yield * this.client.dht.query(keyStr, {
+      for await (const event of this.client.dht.query(keyStr, {
         timeout: options.timeout
-      })
+      })) {
+        if (event.name === 'PEER_RESPONSE') {
+          yield * event.closer.map(closer => ({
+            id: peerIdFromString(closer.id),
+            multiaddrs: closer.multiaddrs.map(ma => new Multiaddr(ma.toString())),
+            protocols: []
+          }))
+        }
+      }
     } catch (err) {
       log.error('getClosestPeers errored:', err)
       throw err
