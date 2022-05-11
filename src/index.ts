31c5db667e70dd17751fb26f39bd58f4ca3db89c
Alex Potsides <alex@achingbrain.net>
2022-05-11 10:08:40 +0100

    fix: abort in flight requests when shutting down (#68)
    Adds a global AbortController that is aborted when shutting down.

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,25 @@ import defer from 'p-defer'
 import errCode from 'err-code'
 import { Multiaddr } from '@multiformats/multiaddr'
 import { peerIdFromString } from '@libp2p/peer-id'
+import anySignal from 'any-signal'
 import type { PeerId } from '@libp2p/interfaces/peer-id'
 import type { IPFSHTTPClient } from 'ipfs-http-client'
 import type { HTTPClientExtraOptions } from 'ipfs-http-client/types/src/types'
 import type { AbortOptions } from 'ipfs-core-types/src/utils'
 import type { PeerRouting } from '@libp2p/interfaces/peer-routing'
 import type { PeerInfo } from '@libp2p/interfaces/peer-info'
+import type { Startable } from '@libp2p/interfaces/startable'
 
 const log = logger('libp2p-delegated-peer-routing')
 
 const DEFAULT_TIMEOUT = 30e3 // 30 second default
 const CONCURRENT_HTTP_REQUESTS = 4
 
-export class DelegatedPeerRouting implements PeerRouting {
+export class DelegatedPeerRouting implements PeerRouting, Startable {
   private readonly client: IPFSHTTPClient
   private readonly httpQueue: PQueue
+  private started: boolean
+  private abortController: AbortController
 
   /**
    * Create a new DelegatedPeerRouting instance
@@ -30,6 +34,8 @@ export class DelegatedPeerRouting implements PeerRouting {
     }
 
     this.client = client
+    this.started = false
+    this.abortController = new AbortController()
 
     // limit concurrency to avoid request flood in web browser
     // https://github.com/libp2p/js-libp2p-delegated-content-routing/issues/12
@@ -46,12 +52,28 @@ export class DelegatedPeerRouting implements PeerRouting {
     log(`enabled DelegatedPeerRouting via ${protocol}://${host}:${port}`)
   }
 
+  isStarted () {
+    return this.started
+  }
+
+  start () {
+    this.started = true
+  }
+
+  stop () {
+    this.httpQueue.clear()
+    this.abortController.abort()
+    this.abortController = new AbortController()
+    this.started = false
+  }
+
   /**
    * Attempts to find the given peer
    */
   async findPeer (id: PeerId, options: HTTPClientExtraOptions & AbortOptions = {}) {
     log('findPeer starts: %p', id)
     options.timeout = options.timeout ?? DEFAULT_TIMEOUT
+    options.signal = anySignal([this.abortController.signal].concat((options.signal != null) ? [options.signal] : []))
 
     const onStart = defer()
     const onFinish = defer()
@@ -64,9 +86,7 @@ export class DelegatedPeerRouting implements PeerRouting {
     try {
       await onStart.promise
 
-      for await (const event of this.client.dht.findPeer(id.toString(), {
-        timeout: options.timeout
-      })) {
+      for await (const event of this.client.dht.findPeer(id.toString(), options)) {
         if (event.name === 'FINAL_PEER') {
           const peerInfo: PeerInfo = {
             id: peerIdFromString(event.peer.id),
@@ -97,6 +117,7 @@ export class DelegatedPeerRouting implements PeerRouting {
 
     log('getClosestPeers starts:', keyStr)
     options.timeout = options.timeout ?? DEFAULT_TIMEOUT
+    options.signal = anySignal([this.abortController.signal].concat((options.signal != null) ? [options.signal] : []))
 
     const onStart = defer()
     const onFinish = defer()
@@ -109,9 +130,7 @@ export class DelegatedPeerRouting implements PeerRouting {
     try {
       await onStart.promise
 
-      for await (const event of this.client.dht.query(keyStr, {
-        timeout: options.timeout
-      })) {
+      for await (const event of this.client.dht.query(keyStr, options)) {
         if (event.name === 'PEER_RESPONSE') {
           yield * event.closer.map(closer => ({
             id: peerIdFromString(closer.id),
